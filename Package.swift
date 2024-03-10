29e9bce0c4185473da51d1bffc86ac7d3858d2e2
kyrylokhlopko <khlopko@gmail.com>
2024-03-10 17:45:15 +0100

    Add Swift bindings for SwiftTreeSitter

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:5.10
+
+import PackageDescription
+
+let package = Package(
+    name: "TreeSitterMarkdown",
+    products: [
+        .library(name: "TreeSitterMarkdown", targets: ["TreeSitterMarkdown"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "TreeSitterMarkdown",
+                path: ".",
+                exclude: [
+                    "binding.gyp",
+                    "bindings",
+                    "Cargo.toml",
+                    "corpus",
+                    "grammar.js",
+                    "LICENSE",
+                    "package.json",
+                    "README.md",
+                    "src/grammar.json",
+                    "src/node-types.json",
+                ],
+                sources: [
+                    "src/parser.c",
+                ],
+                publicHeadersPath: "bindings/swift",
+                cSettings: [.headerSearchPath("src")])
+    ]
+)
